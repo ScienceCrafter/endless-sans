0213d4d74b816f2f8eedd48d70fe252c0f8721cf
ScienceCrafter <42043052+ScienceCrafter@users.noreply.github.com>
2021-02-14 12:34:46 -0500

    Update bonesets.js

--- js/bonesets.js
+++ js/bonesets.js
@@ -24,10 +24,16 @@ var default_bone_set = {
 		[-500, 168, 16, 128, 160],
 		[-500, 24, 16, 128, 160],
 	],
-	next_bone_sets: [
+	if (document.getElementById("game_settings_other").value.includes("only_hurdles") {
+		next_bone_sets: [
+		"4A", "5A"
+		]
+	} else {
+		next_bone_sets: [
 		// "1L", "1R", "2L", "2R", "3",
 		"6L", "6R"
-	],
+		]
+	}
 };
 
 
@@ -196,9 +202,15 @@ var bone_set_4C = {
 		[328, 168, 16, 128, -240],
 		[328, 24, 16, 128, -240],
 	],
-	next_bone_sets: [
+	if (document.getElementById("game_settings_other").value.includes("only_hurdles") {
+		next_bone_sets: [
+		"5A"
+		]
+	} else {
+		next_bone_sets: [
 		"1R"
-	],
+		],
+	}
 };
 
 
@@ -259,9 +271,15 @@ var bone_set_5C = {
 		[-8, 168, 16, 128, 240],
 		[-8, 24, 16, 128, 240],
 	],
-	next_bone_sets: [
+	if (document.getElementById("game_settings_other").value.includes("only_hurdles") {
+		next_bone_sets: [
+		"4A"
+		]
+	} else {
+		next_bone_sets: [
 		"1L"
-	],
+		],
+	}
 };
 
 
